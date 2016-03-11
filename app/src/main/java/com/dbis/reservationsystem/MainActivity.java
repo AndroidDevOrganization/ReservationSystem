33282ad7319aafef3ec1c624ac630e772ce0f914
Swike <swikecc@users.noreply.github.com>
2016-03-11 10:50:16 +0800

    for dev main page and navigation view

--- app/src/main/java/com/dbis/reservationsystem/MainActivity.java
+++ app/src/main/java/com/dbis/reservationsystem/MainActivity.java
@@ -2,20 +2,32 @@ package com.dbis.reservationsystem;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+//    ToggleButton toggleButton;
+//    Switch switcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,6 +38,48 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+//        toggleButton = (ToggleButton) findViewById(R.id.toggle);
+//        switcher = (Switch) findViewById(R.id.switcher);
+//        final LinearLayout test = (LinearLayout) findViewById(R.id.test);
+//        OnCheckedChangeListener listener = new OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked) {
+//                    //设置LinearLayout垂直布局
+//                    test.setOrientation(LinearLayout.VERTICAL);
+//                    toggleButton.setChecked(true);
+//                    switcher.setChecked(true);
+//                }
+//                else {
+//                    //设置LinearLayout水平布局
+//                    test.setOrientation(LinearLayout.HORIZONTAL);
+//                    toggleButton.setChecked(false);
+//                    switcher.setChecked(false);
+//                }
+//            }
+//        };
+//        toggleButton.setOnCheckedChangeListener(listener);
+//        switcher.setOnCheckedChangeListener(listener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_main);
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -43,10 +97,43 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.settings) {
+            return true;
+        } else if (id == R.id.abouts) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+
+        } else if (id == R.id.nav_myReservation) {
+
+        } else if (id == R.id.nav_collection) {
+
+        } else if (id == R.id.nav_theme) {
+
+        } else if (id == R.id.nav_settings) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_main);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void userHandler(View source) {
+
+    }
+
+    public void logout(View source) {
+
     }
 }
